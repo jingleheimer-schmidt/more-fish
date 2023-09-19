4b07566bf0629308328ebde03846733c0a06dcdb
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2023-09-18 19:25:59 -0700

    generate autoplaced fish when installed on existing save file

---
+++ control.lua
@@ -0,0 +1,13 @@
+
+local function on_init()
+    if game.ticks_played > 0 then
+        for _, surface in pairs(game.surfaces) do
+            surface.regenerate_entity("salmon")
+            surface.regenerate_entity("cod")
+            surface.regenerate_entity("pufferfish")
+            surface.regenerate_entity("clownfish")
+        end
+    end
+end
+
+script.on_init(on_init)
