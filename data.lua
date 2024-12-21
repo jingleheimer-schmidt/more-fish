7a4032ef5c951dba9db4d73cece7222484dc965e
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-12-20 22:20:23 -0800

    use deepcopied raw-fish as base

--- data.lua
+++ data.lua
@@ -1,368 +1,188 @@
 
 local sounds = require("__base__.prototypes.entity.sounds")
 
-local salmon = {
-    type = "fish",
-    name = "salmon",
-    icon = "__more-fish__/graphics/icons/Raw_Salmon_JE2_BE2.png",
-    icon_size = 160,
-    flags = { "placeable-neutral", "not-on-map" },
-    minable = { mining_time = 0.4, result = "raw-salmon", count = 5 },
-    mined_sound = sounds.mine_fish,
-    max_health = 20,
-    subgroup = "creatures",
-    order = "b-a-salmon",
-    collision_box = { { -0.75, -0.75 }, { 0.75, 0.75 } },
-    selection_box = { { -0.5, -0.3 }, { 0.5, 0.3 } },
-    pictures =
-    {
-        {
-            filename = "__base__/graphics/entity/fish/fish-1.png",
-            priority = "extra-high",
-            width = 22,
-            height = 36
-        },
-        {
-            filename = "__base__/graphics/entity/fish/fish-2.png",
-            priority = "extra-high",
-            width = 32,
-            height = 32
-        }
-    },
-    autoplace =
-    {
-        order = "x-salmon",
-        peaks =
-        {
-            {
-                influence = 0.01,
-                min_influence = 0,
-                water_optimal = 0.624,
-                water_range = 0.124,
-                water_max_range = 0.124,
+local base_fish = data.raw["fish"]["fish"]
+
+local salmon = table.deepcopy(base_fish)
+salmon.name = "salmon"
+salmon.icon = "__more-fish__/graphics/icons/Raw_Salmon_JE2_BE2.png"
+salmon.icon_size = 160
+salmon.minable.result = "raw-salmon"
+salmon.minable.count = 5
+salmon.order = "b-a-salmon"
+if salmon.autoplace then
+    salmon.autoplace.order = "x-salmon"
+    salmon.autoplace.probability_expression = 0.5
+end
+
+local cod = table.deepcopy(base_fish)
+cod.name = "cod"
+cod.icon = "__more-fish__/graphics/icons/Raw_Cod_JE4_BE2.png"
+cod.icon_size = 160
+cod.minable.result = "raw-cod"
+cod.minable.count = 5
+cod.order = "b-a-cod"
+if cod.autoplace then
+    cod.autoplace.order = "x-cod"
+    cod.autoplace.probability_expression = 0.5
+end
+
+local pufferfish = table.deepcopy(base_fish)
+pufferfish.name = "pufferfish"
+pufferfish.icon = "__more-fish__/graphics/icons/Pufferfish_(item)_JE5_BE2.png"
+pufferfish.icon_size = 160
+pufferfish.minable.result = "raw-pufferfish"
+pufferfish.minable.count = 2
+pufferfish.order = "b-a-pufferfish"
+if pufferfish.autoplace then
+    pufferfish.autoplace.order = "x-pufferfish"
+    pufferfish.autoplace.probability_expression = 0.5
+end
+
+local clownfish = table.deepcopy(base_fish)
+clownfish.name = "clownfish"
+clownfish.icon = "__more-fish__/graphics/icons/Tropical_Fish_JE2_BE2.png"
+clownfish.icon_size = 160
+clownfish.minable.result = "raw-clownfish"
+clownfish.minable.count = 1
+clownfish.order = "b-a-clownfish"
+if clownfish.autoplace then
+    clownfish.autoplace.order = "x-clownfish"
+    clownfish.autoplace.probability_expression = 0.5
+end
+
+local base_capsule = data.raw["capsule"]["raw-fish"]
+
+local salmon_capsule = table.deepcopy(base_capsule)
+salmon_capsule.name = "raw-salmon"
+salmon_capsule.icon = "__more-fish__/graphics/icons/Raw_Salmon_JE2_BE2.png"
+salmon_capsule.icon_size = 160
+salmon_capsule.order = "h[raw-fish][salmon]"
+salmon_capsule.rocket_launch_products = {
+    { type = "item", name = "raw-fish", amount = 1 }
+}
+salmon_capsule.capsule_action = {
+    type = "use-on-self",
+    attack_parameters = {
+        type = "projectile",
+        activation_type = "consume",
+        ammo_category = "capsule",
+        cooldown = 30,
+        range = 0,
+        ammo_type = {
+            category = "capsule",
+            target_type = "position",
+            action = {
+                type = "direct",
+                action_delivery = {
+                    type = "instant",
+                    target_effects = {
+                        { type = "damage", damage = { type = "physical", amount = -80 } },
+                        { type = "play-sound", sound = sounds.eat_fish }
+                    }
+                }
             }
         }
     }
 }
 
-local cod = {
-    type = "fish",
-    name = "cod",
-    icon = "__more-fish__/graphics/icons/Raw_Cod_JE4_BE2.png",
-    icon_size = 160,
-    flags = { "placeable-neutral", "not-on-map" },
-    minable = { mining_time = 0.4, result = "raw-cod", count = 5 },
-    mined_sound = sounds.mine_fish,
-    max_health = 20,
-    subgroup = "creatures",
-    order = "b-a-cod",
-    collision_box = { { -0.75, -0.75 }, { 0.75, 0.75 } },
-    selection_box = { { -0.5, -0.3 }, { 0.5, 0.3 } },
-    pictures =
-    {
-        {
-            filename = "__base__/graphics/entity/fish/fish-1.png",
-            priority = "extra-high",
-            width = 22,
-            height = 36
-        },
-        {
-            filename = "__base__/graphics/entity/fish/fish-2.png",
-            priority = "extra-high",
-            width = 32,
-            height = 32
-        }
-    },
-    autoplace =
-    {
-        order = "x-cod",
-        peaks =
-        {
-            {
-                influence = 0.01,
-                min_influence = 0,
-                water_optimal = 0.876,
-                water_range = 0.126,
-                water_max_range = 0.126,
+local cod_capsule = table.deepcopy(base_capsule)
+cod_capsule.name = "raw-cod"
+cod_capsule.icon = "__more-fish__/graphics/icons/Raw_Cod_JE4_BE2.png"
+cod_capsule.icon_size = 160
+cod_capsule.order = "h[raw-fish][cod]"
+cod_capsule.rocket_launch_products = {
+    { type = "item", name = "raw-fish", amount = 1 }
+}
+cod_capsule.capsule_action = {
+    type = "use-on-self",
+    attack_parameters = {
+        type = "projectile",
+        activation_type = "consume",
+        ammo_category = "capsule",
+        cooldown = 30,
+        range = 0,
+        ammo_type = {
+            category = "capsule",
+            target_type = "position",
+            action = {
+                type = "direct",
+                action_delivery = {
+                    type = "instant",
+                    target_effects = {
+                        { type = "damage", damage = { type = "physical", amount = -80 } },
+                        { type = "play-sound", sound = sounds.eat_fish }
+                    }
+                }
             }
         }
     }
 }
 
-local pufferfish = {
-    type = "fish",
-    name = "pufferfish",
-    icon = "__more-fish__/graphics/icons/Pufferfish_(item)_JE5_BE2.png",
-    icon_size = 160,
-    flags = { "placeable-neutral", "not-on-map" },
-    minable = { mining_time = 0.4, result = "raw-pufferfish", count = 2 },
-    mined_sound = sounds.mine_fish,
-    max_health = 20,
-    subgroup = "creatures",
-    order = "b-a-pufferfish",
-    collision_box = { { -0.75, -0.75 }, { 0.75, 0.75 } },
-    selection_box = { { -0.5, -0.3 }, { 0.5, 0.3 } },
-    pictures =
-    {
-        {
-            filename = "__base__/graphics/entity/fish/fish-1.png",
-            priority = "extra-high",
-            width = 22,
-            height = 36
-        },
-        {
-            filename = "__base__/graphics/entity/fish/fish-2.png",
-            priority = "extra-high",
-            width = 32,
-            height = 32
-        }
-    },
-    autoplace =
-    {
-        order = "x-pufferfish",
-        peaks =
-        {
-            {
-                influence = 0.01,
-                min_influence = 0,
-                water_optimal = 0.252,
-                water_range = 0.252,
-                water_max_range = 0.252,
+local pufferfish_capsule = table.deepcopy(base_capsule)
+pufferfish_capsule.name = "raw-pufferfish"
+pufferfish_capsule.icon = "__more-fish__/graphics/icons/Pufferfish_(item)_JE5_BE2.png"
+pufferfish_capsule.icon_size = 160
+pufferfish_capsule.order = "h[raw-fish][pufferfish]"
+pufferfish_capsule.rocket_launch_products = {
+    { type = "item", name = "raw-fish", amount = 1 }
+}
+pufferfish_capsule.capsule_action = {
+    type = "use-on-self",
+    attack_parameters = {
+        type = "projectile",
+        activation_type = "consume",
+        ammo_category = "capsule",
+        cooldown = 30,
+        range = 0,
+        ammo_type = {
+            category = "capsule",
+            target_type = "position",
+            action = {
+                type = "direct",
+                action_delivery = {
+                    type = "instant",
+                    target_effects = {
+                        { type = "damage", damage = { type = "physical", amount = 80 } },
+                        { type = "play-sound", sound = sounds.eat_fish }
+                    }
+                }
             }
         }
     }
 }
 
-local clownfish = {
-    type = "fish",
-    name = "clownfish",
-    icon = "__more-fish__/graphics/icons/Tropical_Fish_JE2_BE2.png",
-    icon_size = 160,
-    flags = { "placeable-neutral", "not-on-map" },
-    minable = { mining_time = 0.4, result = "raw-clownfish", count = 1 },
-    mined_sound = sounds.mine_fish,
-    max_health = 20,
-    subgroup = "creatures",
-    order = "b-a-clownfish",
-    collision_box = { { -0.75, -0.75 }, { 0.75, 0.75 } },
-    selection_box = { { -0.5, -0.3 }, { 0.5, 0.3 } },
-    pictures =
-    {
-        {
-            filename = "__base__/graphics/entity/fish/fish-1.png",
-            priority = "extra-high",
-            width = 22,
-            height = 36
-        },
-        {
-            filename = "__base__/graphics/entity/fish/fish-2.png",
-            priority = "extra-high",
-            width = 32,
-            height = 32
-        }
-    },
-    autoplace =
-    {
-        order = "x-clownfish",
-        peaks =
-        {
-            {
-                influence = 0.01,
-                min_influence = 0,
-                water_optimal = 0.261,
-                water_range = 0.261,
-                water_max_range = 0.261,
+local clownfish_capsule = table.deepcopy(base_capsule)
+clownfish_capsule.name = "raw-clownfish"
+clownfish_capsule.icon = "__more-fish__/graphics/icons/Tropical_Fish_JE2_BE2.png"
+clownfish_capsule.icon_size = 160
+clownfish_capsule.order = "h[raw-fish][clownfish]"
+clownfish_capsule.rocket_launch_products = {
+    { type = "item", name = "raw-fish", amount = 1 }
+}
+clownfish_capsule.capsule_action = {
+    type = "use-on-self",
+    attack_parameters = {
+        type = "projectile",
+        activation_type = "consume",
+        ammo_category = "capsule",
+        cooldown = 30,
+        range = 0,
+        ammo_type = {
+            category = "capsule",
+            target_type = "position",
+            action = {
+                type = "direct",
+                action_delivery = {
+                    type = "instant",
+                    target_effects = {
+                        { type = "damage", damage = { type = "physical", amount = -100 } },
+                        { type = "play-sound", sound = sounds.eat_fish }
+                    }
+                }
             }
         }
     }
-}
-
-local salmonItem = {
-    type = "capsule",
-    name = "raw-salmon",
-    icon = "__more-fish__/graphics/icons/Raw_Salmon_JE2_BE2.png",
-    icon_size = 160,
-    subgroup = "raw-resource",
-    capsule_action =
-    {
-        type = "use-on-self",
-        attack_parameters =
-        {
-            type = "projectile",
-            activation_type = "consume",
-            ammo_category = "capsule",
-            cooldown = 30,
-            range = 0,
-            ammo_type =
-            {
-                category = "capsule",
-                target_type = "position",
-                action =
-                {
-                    type = "direct",
-                    action_delivery =
-                    {
-                        type = "instant",
-                        target_effects =
-                        {
-                            {
-                                type = "damage",
-                                damage = { type = "physical", amount = -80 }
-                            },
-                            {
-                                type = "play-sound",
-                                sound = sounds.eat_fish
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    },
-    order = "h[raw-fish][salmon]",
-    rocket_launch_product = { "raw-fish", 1 },
-    stack_size = 100
-}
-
-local codItem = {
-    type = "capsule",
-    name = "raw-cod",
-    icon = "__more-fish__/graphics/icons/Raw_Cod_JE4_BE2.png",
-    icon_size = 160,
-    subgroup = "raw-resource",
-    capsule_action =
-    {
-        type = "use-on-self",
-        attack_parameters =
-        {
-            type = "projectile",
-            activation_type = "consume",
-            ammo_category = "capsule",
-            cooldown = 30,
-            range = 0,
-            ammo_type =
-            {
-                category = "capsule",
-                target_type = "position",
-                action =
-                {
-                    type = "direct",
-                    action_delivery =
-                    {
-                        type = "instant",
-                        target_effects =
-                        {
-                            {
-                                type = "damage",
-                                damage = { type = "physical", amount = -80 }
-                            },
-                            {
-                                type = "play-sound",
-                                sound = sounds.eat_fish
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    },
-    order = "h[raw-fish][cod]",
-    rocket_launch_product = { "raw-fish", 1 },
-    stack_size = 100
-}
-
-local pufferfishItem = {
-    type = "capsule",
-    name = "raw-pufferfish",
-    icon = "__more-fish__/graphics/icons/Pufferfish_(item)_JE5_BE2.png",
-    icon_size = 160,
-    subgroup = "raw-resource",
-    capsule_action =
-    {
-        type = "use-on-self",
-        attack_parameters =
-        {
-            type = "projectile",
-            activation_type = "consume",
-            ammo_category = "capsule",
-            cooldown = 30,
-            range = 0,
-            ammo_type =
-            {
-                category = "capsule",
-                target_type = "position",
-                action =
-                {
-                    type = "direct",
-                    action_delivery =
-                    {
-                        type = "instant",
-                        target_effects =
-                        {
-                            {
-                                type = "damage",
-                                damage = { type = "physical", amount = 80 }
-                            },
-                            {
-                                type = "play-sound",
-                                sound = sounds.eat_fish
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    },
-    order = "h[raw-fish][pufferfish]",
-    rocket_launch_product = { "raw-fish", 1 },
-    stack_size = 100
-}
-
-local clownfishItem = {
-    type = "capsule",
-    name = "raw-clownfish",
-    icon = "__more-fish__/graphics/icons/Tropical_Fish_JE2_BE2.png",
-    icon_size = 160,
-    subgroup = "raw-resource",
-    capsule_action =
-    {
-        type = "use-on-self",
-        attack_parameters =
-        {
-            type = "projectile",
-            activation_type = "consume",
-            ammo_category = "capsule",
-            cooldown = 30,
-            range = 0,
-            ammo_type =
-            {
-                category = "capsule",
-                target_type = "position",
-                action =
-                {
-                    type = "direct",
-                    action_delivery =
-                    {
-                        type = "instant",
-                        target_effects =
-                        {
-                            {
-                                type = "damage",
-                                damage = { type = "physical", amount = -100 }
-                            },
-                            {
-                                type = "play-sound",
-                                sound = sounds.eat_fish
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    },
-    order = "h[raw-fish][clownfish]",
-    rocket_launch_product = { "raw-fish", 1 },
-    stack_size = 100
 }
 
 data:extend({
@@ -370,8 +190,8 @@ data:extend({
     cod,
     clownfish,
     pufferfish,
-    salmonItem,
-    codItem,
-    pufferfishItem,
-    clownfishItem
+    salmon_capsule,
+    cod_capsule,
+    pufferfish_capsule,
+    clownfish_capsule
 })
